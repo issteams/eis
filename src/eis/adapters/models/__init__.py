c9ee744b9cb5b96fbe05c61472bf003935560d77
Abdulmumin Abubakar <114495965+codesmith-abba@users.noreply.github.com>
2026-09-16 22:33:01 +0100

    feat: export provider factory

--- src/eis/adapters/models/__init__.py
+++ src/eis/adapters/models/__init__.py
@@ -1,5 +1,6 @@
 """Concrete model provider adapters."""
 
 from eis.adapters.models.openai_compatible import OpenAICompatibleModel
+from eis.adapters.models.providers import OpenAICompatibleProvider
 
-__all__ = ["OpenAICompatibleModel"]
+__all__ = ["OpenAICompatibleModel", "OpenAICompatibleProvider"]
