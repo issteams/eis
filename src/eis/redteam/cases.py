c07dc67f8bc5170c9884d09093d81e6c7ea4d81e
Abdulmumin Abubakar <114495965+codesmith-abba@users.noreply.github.com>
2026-09-17 09:16:42 +0100

    fix redteam case import ordering

--- src/eis/redteam/cases.py
+++ src/eis/redteam/cases.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from eis.redteam.models import AttackCategory, AttackCase, AttackSeverity
+from eis.redteam.models import AttackCase, AttackCategory, AttackSeverity
 
 
 CASES: tuple[AttackCase, ...] = (
